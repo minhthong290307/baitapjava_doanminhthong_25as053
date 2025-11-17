d44ea1894e7ba191c249f12593e31a931ad64469
Thongdoan <minhthong290307@gmail.com>
2025-11-17 09:59:10 +0700

    commit ex 1.2

---
+++ src/ex12/Circle.java
@@ -0,0 +1,35 @@
+package ex12;
+
+public class Circle {
+    private double radius = 1.0;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle() {
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getCircumference() {
+        return radius * 2 * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
